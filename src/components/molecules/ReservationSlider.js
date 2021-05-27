a36c83ddc085b9d1f0c30d1f8497613b79f84d5a
michrytel <76482065+michrytel@users.noreply.github.com>
2021-05-27 22:48:50 +0200

    reservation slider with animation

---
+++ src/components/molecules/ReservationSlider.js
@@ -0,0 +1,38 @@
+import checkmark from "../../assets/icons/ui/check.svg"
+import {Swiper, SwiperSlide} from "swiper/react";
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css"
+import SwiperCore, {
+    Autoplay,Pagination,Navigation
+} from 'swiper/core';
+SwiperCore.use([Autoplay,Pagination,Navigation]);
+const list = ["konsultacja bez zakadania konta", "pelna dyskrecja", "kontakt przez Skype lub WhatsApp", "rezerwacja terminu on-line", "szybkosc i prostota", "video konsultacja", "e-recepta i e-zwolnienie"]
+
+const ReservationSlider = () => {
+    return (
+        <div className="reservation__slider">
+            <Swiper direction={"vertical"} slidesPerView={7.2} spaceBetween={10} loop={true} centeredSlides={true} effect={"fade"} autoplay={{
+                "delay": 2500,
+                "disableOnInteraction": false
+            }} pagination={{
+                "clickable": true,
+                el: '.swiper-pagination',
+            }} fadeEffect={{
+                crossFade:true
+            }} navigation={true} className="mySwiper">
+                {list.map((el, i) =>
+                    <SwiperSlide>
+                        <div className='slide' key={i}>
+                            <div className="checkmark">
+                                <img src={checkmark} alt="checkmark"/>
+                            </div>
+                            <p>{el}</p>
+                        </div>
+                    </SwiperSlide>
+                )}
+            </Swiper>
+        </div>
+    )
+}
+
+export default ReservationSlider
--- src/components/organisms/Reservation.js
+++ src/components/organisms/Reservation.js
@@ -1,8 +1,7 @@
 import Headline from "../atoms/Headline";
 import Button from "../atoms/Button";
-import checkmark from "../../assets/icons/ui/check.svg"
+import ReservationSlider from "../molecules/ReservationSlider";
 
-const list = ["konsultacja bez zakadania konta", "pelna dyskrecja", "kontakt przez Skype lub WhatsApp", "rezerwacja terminu on-line", "szybkosc i prostota", "video konsultacja", "e-recepta i e-zwolnienie"]
 const Reservation = () => {
     return (
         <section className="reservation">
@@ -11,16 +10,7 @@ const Reservation = () => {
                     <Headline title="Zapisz sie na wizyte on-line"/>
                     <Button text="Zarezerwuj termin już teraz" style="button__secondary"/>
                 </div>
-                <div className="reservation__slider">
-                    {list.map((el, i) =>
-                        <div className={`slide key${i}`} key={i}>
-                            <div className="checkmark">
-                                <img src={checkmark} alt="checkmark"/>
-                            </div>
-                            <p>{el}</p>
-                        </div>
-                    )}
-                </div>
+                  <ReservationSlider/>
             </div>
         </section>
     )
