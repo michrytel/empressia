35dee4055e37e6f66ef2fb2d5fc0fbc678717d0f
michrytel <76482065+michrytel@users.noreply.github.com>
2021-05-27 15:28:01 +0200

    slider components

--- src/components/atoms/Review.js
+++ src/components/atoms/Review.js
@@ -1,4 +1,27 @@
-const Review = () => {
-
+import userImage from "../../assets/icons/ui/eye.svg";
+const Review = ({data, index}) => {
+    return (
+        <div className="review__container" key={index}>
+            <div className="review__info">
+                <div className="rating">
+                    {data.stars}
+                </div>
+                <div className="date">
+                    {data.date}
+                </div>
+            </div>
+            <div className="review">
+                {data.review}
+            </div>
+            <div className="user">
+                <div className="user__image">
+                    <img src={userImage}/>
+                </div>
+                <div className="user__name">
+                    {data.user}
+                </div>
+            </div>
+        </div>
+    )
 }
 export default Review
--- src/components/molecules/ReviewsSlider.js
+++ src/components/molecules/ReviewsSlider.js
@@ -1,8 +1,23 @@
 import Review from "../atoms/Review";
+import {Swiper, SwiperSlide} from "swiper/react";
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css"
+import SwiperCore, {Pagination} from 'swiper/core';
+import reviewsData from "../../assets/Reviews.json"
+
+SwiperCore.use([Pagination]);
 
 const ReviewsSlider = () => {
     return (
-        <Review/>
+        <Swiper slidesPerView={4} centeredSlides={false} spaceBetween={30} grabCursor={true} pagination={{
+            "clickable": true
+        }} className="mySwiper">
+            {reviewsData.map((review, i) =>
+                <SwiperSlide>
+                    <Review data={review} index={i}/>
+                </SwiperSlide>
+            )}
+        </Swiper>
     )
 }
 export default ReviewsSlider
